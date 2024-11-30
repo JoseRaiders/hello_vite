376493f49ecc65d210cf50878538d452054ceef9
JoseRaiders <71295774+JoseRaiders@users.noreply.github.com>
2024-11-30 14:37:27 -0600

    add default image

--- src/components/Animal.jsx
+++ src/components/Animal.jsx
@@ -2,9 +2,16 @@ import { data } from "../utils/constants.js";
 
 function Animal({ selectedAnimal }) {
   // this is the props
-  const animal = data.find((item) => {
+  let animal = data.find((item) => {
     return item.name == selectedAnimal;
   });
+
+  // before rendering, check if it existing
+  animal = animal?.image
+    ? animal
+    : data.find((item) => {
+        return item.name == "default";
+      });
 
   return (
     <img src={animal.image} alt={animal.name} height="300px" width="300px" />
